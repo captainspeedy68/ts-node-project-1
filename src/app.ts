e20891fb65a894217948d1f748637a440d3f44fa
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-22 00:56:38 +0600

    service app and configuration

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,29 @@
+
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { StudentRoutes } from './app/modules/student/student.route'
+import { StudentControllers } from './app/modules/student/student.controller'
 const app = express()
 // const port = 3000
 
 app.use(express.json())
 app.use(express.text())
 app.use(cors())
+app.use(express.Router())
 
-// app.use(express.Router)
-
-app.get('/', (req: Request, res: Response) => {
+const getAController = (req: Request, res: Response) => {
   // const a = "something";
-
   res.send('Hello World!')
-})
+  console.log("running")
+  
+}
+
+// application routes
+app.use("/api/v1/students", StudentRoutes);
+
+// app.get("/api/v1/students", StudentControllers.getStudent)
+
+app.get('/', getAController)
+// app.get('/api/v1/students', getAController)
 
 export default app
